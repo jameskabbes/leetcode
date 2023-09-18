1f5d29bdd3d7126abe3ca5c19811f8d993f1ae23
James Kabbes <james.kabbes@gmail.com>
2023-09-18 11:28:40 -0500

    prepping dev env

---
+++ 23/test.py
@@ -0,0 +1,10 @@
+from solution import ListNode, Solution
+
+result = Solution().mergeKLists(
+    [
+        ListNode( 1, ListNode(4, ListNode(5)) ),
+        ListNode( 1, ListNode(3, ListNode(4)) ),
+        ListNode( 2, ListNode(6))
+    ])
+
+print (result)
