59e3100a5041441b56a1d75dfc51ee74bae2266c
James Kabbes <james.kabbes@gmail.com>
2023-10-13 16:57:23 -0500

    setting up baseline solution

---
+++ 51/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        
+        ### should probably use recursion to solve this
+        pass
+        
+        
+        
+        
+        
+print (Solution().solveNQueens( 4 ))
