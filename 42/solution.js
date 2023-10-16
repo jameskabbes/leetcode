6f39b0429b5142104a0b9ba481fd90f10240b9e5
James Kabbes <james.kabbes@gmail.com>
2023-10-16 13:14:20 -0500

    adding starter code

---
+++ 42/solution.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    return 6
+};
+
+console.log( trap( [0,1,0,2,1,0,1,3,2,1,2,1] ) )
---
+++ 42/solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        return 6
+        
+print (Solution().trap( [0,1,0,2,1,0,1,3,2,1,2,1] ))
