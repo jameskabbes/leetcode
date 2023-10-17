b677eb3c4730dc5b1687ab5c975eee416f1e2bf9
James Kabbes <james.kabbes@gmail.com>
2023-10-17 16:13:57 -0500

    faster solution

--- 11/solution.py
+++ 11/solution.py
@@ -1,17 +1,24 @@
 from typing import List
 
-def get_area( index1, index2, height1, height2 ):
-    return abs( index2-index1 ) * min(height1, height2)
+def get_area( index1, index2, height ):
+    return abs(index2-index1)*( min(height[index1], height[index2]) )
+
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
 
+        left_ind = 0
+        right_ind = len(height)-1
+
         max_area = 0
-        for i in range(len(height)-1):
-            for j in range( 1, len(height) ):
-                area = get_area( i, j, height[i], height[j] )
-                max_area = max( area, max_area )        
+        while left_ind < right_ind:
+            max_area = max( max_area, get_area( left_ind, right_ind, height ) )
 
+            if height[left_ind] < height[right_ind]:
+                left_ind += 1
+            else:
+                right_ind -= 1
+        
         return max_area
-
+    
 print (Solution().maxArea([1,8,6,2,5,4,8,3,7]))
