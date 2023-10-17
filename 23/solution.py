1f88a82eb8198199a2e438dba6778fabbd2f3d4f
James Kabbes <james.kabbes@gmail.com>
2023-10-17 15:42:08 -0500

    solved

--- 23/solution.py
+++ 23/solution.py
@@ -24,19 +24,21 @@ class Solution:
             if lists[i] != None:
                 if lists[i].val != None:
                     if lists[i].val not in header_vals:
-                        header_vals[ lists[i].val ] = [ i ]
+                        header_vals[ lists[i].val ] = {i}
                     else:
-                        header_vals[ lists[i].val ].append( i )
+                        header_vals[ lists[i].val ].add(i)
 
         return self.merge( lists, header_vals )
 
-    def merge( self, lists, header_vals ):
+    def merge( self, lists, header_vals, min_header_val=None ):
 
         print (header_vals)
         if len(header_vals) == 0:
             return None
 
-        min_header_val = min(header_vals)
+        if min_header_val == None:
+            min_header_val = min(header_vals)
+
         list_index = header_vals[ min_header_val ].pop()
 
         # get the next node in the list
@@ -45,12 +47,15 @@ class Solution:
         
         if next != None:
             if next.val not in header_vals:
-                header_vals[ next.val ] = [ list_index ]
+                header_vals[ next.val ] = { list_index }
             else:
-                header_vals[ next.val ].append( list_index )
+                header_vals[ next.val ].add( list_index )
 
         # delete the list index from the dict keys if its empty
+        next_header_val = None
         if len(header_vals[ min_header_val ]) == 0:
             del header_vals[min_header_val]
+        else:
+            next_header_val = min_header_val
 
-        return ListNode( val=min_header_val, next=self.merge( lists, header_vals ) )
+        return ListNode( val=min_header_val, next=self.merge( lists, header_vals, min_header_val=next_header_val ) )
