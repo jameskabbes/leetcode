cda721a04b286df042cd6079fd28aff2c371fa87
James Kabbes <james.kabbes@gmail.com>
2023-10-18 15:58:37 -0500

    setup

---
+++ 30/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        
+        n = len(words)
+        word_len = len(words[0])
+        
+        return [0,9]
+        
+
+print (Solution().findSubstring( "barfoothefoobarman", ["foo","bar"] ))
+        
