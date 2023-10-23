6df119dfeb401880c00553fb98618bb4cfe810fe
James Kabbes <james.kabbes@gmail.com>
2023-10-23 16:07:52 -0500

    solved with all solutions

--- 51/solution.py
+++ 51/solution.py
@@ -1,42 +1,36 @@
 from typing import List
-import numpy as np
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
 
-        grid = np.full( (n,n), '.' )
-        return self.solve( grid )
+        grid = []
+        for i in range(n):
+            grid.append( ['.'] * n )
+
+        return self.solve( grid, [] )[-1]
     
-    def solve( self, grid, solutions=[], row=0 ):
-        
-        print ('calling solve')
+    def solve( self, grid, solutions, row=0 ):
         
         if row >= len(grid):
-            return (True, grid)
+            return (True, grid, solutions)
         
         for col in range( len(grid) ):
-            print (grid)
-            print (row,col)
+
             # We can place a queen at this location                
             if self.is_col_viable( grid, col, row ):
 
                 grid[ row ][ col ] = 'Q'
-                valid, grid = self.solve( grid, row=row+1 )
+                valid, grid, solutions = self.solve( grid, solutions, row=row+1 )
 
-                # valid solution, keep on going!
+                # a valid solution
                 if valid:
-                    solutions.append( grid )
-                    return (True, grid)
+                    solutions.append( [ ''.join(grid[i]) for i in range(len(grid))   ] )
 
-                # not a valid solution, try the next column in this row
-                else:
-                    print ('DEAD END')
-                    print (row, col)
-                    grid[row][col] = '.'
-
+                # keep going through the possibilities
+                grid[row][col] = '.'
 
         # we got through the column, no valid solutions available
-        return (False, grid)
+        return (False, grid, solutions)
         
 
     def is_col_viable( self, grid, col, current_row ) -> bool:
@@ -61,7 +55,4 @@ class Solution:
         
         return True
         
-        
-        
-        
-print (Solution().solveNQueens( 6 ))
+print (Solution().solveNQueens( 5 ))
